9d395ee3adcd040876e3077e4aa5004b42559d7f
ciaranoceallaigh <71128211+ciaranoceallaigh96@users.noreply.github.com>
2021-09-27 16:29:35 -0300

    Added make-grm

--- pathway.sh
+++ pathway.sh
@@ -3,3 +3,7 @@ python machine_learning2/cv_saver.py 5 /home/ciaran/completed_big_matrix_binary_
 
 for i in {1..5}; do plink2 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids --indep-pairwise 250 5 0.05 --mac 6 --out /external_s
 torage/ciaran/greml/indep_snps_full_dataset_new_snp_ids_cv_"$i" --keep ~/arabadopsis/2021/train_split_cv_"$i".txt ; done
+
+for i in {1..5}; do /external_storage/eoin/GCTA_manual_install/gcta64 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids \
+--extract /external_storage/ciaran/greml/indep_snps_full_dataset_new_snp_ids_cv_"$i" --keep  ~/arabadopsis/2021/train_split_cv_"$i".txt --make-grm --thread-num 32 \
+--remove /home/ciaran/arabadopsis/completed_big_matrix_binary_new_snps_fail_ids --out /external_storage/ciaran/greml/completed_big_matrix_binary_grm_cv_sep_"$i ; done
