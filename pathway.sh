252132d5b29f440a99adf087f13683e2dc396c05
ciaranoceallaigh <71128211+ciaranoceallaigh96@users.noreply.github.com>
2021-09-27 16:30:54 -0300

    typos

--- pathway.sh
+++ pathway.sh
@@ -5,5 +5,5 @@ for i in {1..5}; do plink2 --bfile /home/ciaran/completed_big_matrix_binary_new_
 torage/ciaran/greml/indep_snps_full_dataset_new_snp_ids_cv_"$i" --keep ~/arabadopsis/2021/train_split_cv_"$i".txt ; done
 
 for i in {1..5}; do /external_storage/eoin/GCTA_manual_install/gcta64 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids \
---extract /external_storage/ciaran/greml/indep_snps_full_dataset_new_snp_ids_cv_"$i" --keep  ~/arabadopsis/2021/train_split_cv_"$i".txt --make-grm --thread-num 32 \
---remove /home/ciaran/arabadopsis/completed_big_matrix_binary_new_snps_fail_ids --out /external_storage/ciaran/greml/completed_big_matrix_binary_grm_cv_sep_"$i ; done
+--extract /external_storage/ciaran/greml/indep_snps_full_dataset_new_snp_ids_cv_"$i".prune.in --keep  ~/arabadopsis/2021/train_split_cv_"$i".txt --make-grm --thread-num 32 \
+--remove /home/ciaran/arabadopsis/completed_big_matrix_binary_new_snps_fail_ids --out /external_storage/ciaran/greml/completed_big_matrix_binary_grm_cv_sep_"$i" ; done
