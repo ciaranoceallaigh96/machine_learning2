024e5d8973411e950d2a5b04bf772f035ccce324
“CIARAN_KELLY” <OCEALLC@TCD.IE>
2021-10-01 21:29:24 +0100

    Flushing out bash script

--- bash_script.sh
+++ bash_script.sh
@@ -1,9 +1,24 @@
-awk '{print $1, $1}' $1 > "$1"_double.txt
-plink2 --assoc --bfile --extract --out 
-#clump
-plink2 --clump --params --out
-head -n 10000 X.prune.in | awk '{print $X}' > top10ksnps.txt
-#extract top snps
-plink1.9 --bfile --keep --extract --recode A --out 
+#phenotype file is first argument e.g /home/ciaran/arabadopsis/phenotypes/values_FT16.8424.80.del
+#cleanup
+rm test_raw_plink* ; train_raw_plink*
 
+#conduct GWAS
+plink2 --glm --mac 20 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids --extract --out nested_cv_gwas_to_delete --keep name_vector_train.txt --pheno $1 
+
+cat header.txt <(sort -g -k 12,12 'nested_cv_gwas_to_delete."FT16".glm.linear' | awk '{if ($12 != "NA") print}' | tail -n +2) > gwas_results.gsorted #formatting
+
+#clump
+plink1.9 --bfile /home/alexg/hopefully_final/completed_big_matrix_binary_new_snps_ids --clump-kb 250 --clump-p1 1 --clump-p2 1 --clump-r2 0.1 --clump gwas_results.gsorted --out gwas_results_clumped
+
+head -n 10000 gwas_results_clumped.clumped | awk '{print $3}'  > top10ksnps.txt
+#extract top snps
+plink1.9 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids --keep name_vector_train.txt --extract top10ksnps.txt --recode A --out train_raw_plink
+
+plink1.9 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids --keep name_vector_test.txt --extract top10ksnps.txt --recode A --out test_raw_plink
+
+
+
+rm name_vector_train.txt ; rm name_vector_test.txt
+rm nested_cv_gwas_to_delete.*
+rm gwas_results*
 
