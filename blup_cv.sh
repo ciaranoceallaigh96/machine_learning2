b566e15d6b6209ace0aaadb8945d455863b85ed2
“CIARAN_KELLY” <OCEALLC@TCD.IE>
2021-12-04 01:09:30 +0000

    debugging

--- blup_cv.sh
+++ blup_cv.sh
@@ -1,8 +1,10 @@
-for i in {1..1} ; do 
-cut -d ' ' -f 1-2 train_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadopsis/phenotypes/train_ids.txt \
+pheno=$1
+
+for i in {1..1} ; do \
+cut -d ' ' -f 1-2 train_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadopsis/phenotypes/train_ids.txt ; \
 cut -d ' ' -f 1-2 test_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadopsis/phenotypes/test_ids.txt \
 ; \
-#Construct GRM \
+\ #Construct GRM \
 /external_storage/eoin/GCTA_manual_install/gcta64 \
 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids \
 --extract shuf_10000_snps_1_in_4_out.txt \
@@ -12,7 +14,7 @@ cut -d ' ' -f 1-2 test_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadops
 --keep /home/ciaran/arabadopsis/phenotypes/train_ids.txt \
 #--remove /home/ciaran/arabadopsis/phenotypes/values_"$pheno".nofailids.txt \
 ; \
-#To obtain BLUP solutions for the genetic values of individuals \
+\ #To obtain BLUP solutions for the genetic values of individuals \
 --reml \
 --grm /external_storage/ciaran/greml/completed_big_matrix_binary_grm_"$pheno"_train_cv_"$i" \
 --pheno /home/ciaran/arabadopsis/phenotypes/values_"$pheno" \
@@ -28,7 +30,7 @@ cut -d ' ' -f 1-2 test_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadops
 --keep /home/ciaran/arabadopsis/phenotypes/train_ids.txt \
 --out /external_storage/ciaran/greml/"$pheno"_gblup_snp_FX_train_only_grm_cv_"$i" \
 ; \
-#To compute the polygenic risk score (PRS) in an independent sample \
+#To compute the polygenic risk score in an independent sample \
 --bfile /home/ciaran/completed_big_matrix_binary_new_snps_ids \
 --keep /home/ciaran/arabadopsis/phenotypes/test_ids.txt \
 --out /external_storage/ciaran/greml/"$pheno"_gblup_train_only_grm_cv_"$i"_test \
