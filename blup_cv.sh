735ef1774753a79bb0e0f8a0fcc700bdd57b37b5
“CIARAN_KELLY” <OCEALLC@TCD.IE>
2021-12-06 12:57:02 +0000

    some fixes

--- blup_cv.sh
+++ blup_cv.sh
@@ -1,6 +1,6 @@
 pheno=$1
 
-for i in {2..4} ; do \
+for i in {1..4} ; do \
 cut -d ' ' -f 1-2 train_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadopsis/phenotypes/train_ids.txt ; \
 cut -d ' ' -f 1-2 test_raw_plink_shuf_"$i"_in_4_out.raw > /home/ciaran/arabadopsis/phenotypes/test_ids.txt \
 ; \
@@ -34,5 +34,5 @@ plink1.9 \
 --pheno /home/ciaran/arabadopsis/phenotypes/values_"$pheno" \
 --score ./"$pheno"_gblup_snp_FX_train_only_grm_cv_"$i".snp.blp 1 2 3 \
 ; \
-python /external_storage/ciaran/machine_learning2/r2_score.py /external_storage/ciaran/greml/"$pheno"_gblup_train_only_grm_cv_"$i"_test.profile "$i" >> "$pheno"_gblup_train_only_grm_cv_test.prscores \
+python /external_storage/ciaran/machine_learning2/r2_score.py ./"$pheno"_gblup_train_only_grm_cv_"$i"_test.profile "$i" >> "$pheno"_gblup_train_only_grm_cv_test.prscores \
 ; done
